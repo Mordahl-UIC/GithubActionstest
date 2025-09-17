5f8f9f7edb18059496f2132dce0ece117b02fb6c
Austin Mordahl <amordahl@uic.edu>
2025-09-17 10:58:03 -0500

    Update tests

--- src/test/scala/edu/uic/MainTests.scala
+++ src/test/scala/edu/uic/MainTests.scala
@@ -4,6 +4,10 @@ import munit.{FunSuite, ScalaCheckSuite}
 // For more information on writing tests, see
 // https://scalameta.org/munit/docs/getting-started.html
 class MySuite extends FunSuite:
+  val macOsSpecific = munit.Tag("macos")
   test("Basic math"):
     assert(1 + 1 == 2)
+
+  test("Only run on MacOS".tag(macOsSpecific)):
+    assert(System.getProperty("os.name").toLowerCase.contains("linux"))
 end MySuite
